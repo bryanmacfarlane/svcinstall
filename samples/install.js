9c467b6bab883b9d3008e3af50be039c93692fea
Bryan MacFarlane <bryanmacf@gmail.com>
2015-01-29 13:31:14 -0500

    osx supports running service as a user

--- samples/install.js
+++ samples/install.js
@@ -2,14 +2,24 @@ var shell = require('shelljs');
 var path = require('path');
 var sim = require('../lib/svcinstall');
 
+// node install.js username
+// if username not supplied will run as root
+var user = process.argv[2];
+
 var nodePath = shell.which('nodejs') || shell.which('node');
 var scriptPath = path.join(__dirname, 'server.js');
 
 var options = { 
 		args: [nodePath, scriptPath],
 		workingDirectory: path.dirname(scriptPath),
+		userName: process.env.USER,
 		env: { "ENV_VAR1": "Value 1", "ENV_VAR2": "Value 2",}
 	};
+
+
+if (user) {
+	options.userName = user;
+}
 
 var svcinstall = new sim.SvcInstall('myserver', 'com.sample');
 svcinstall.install(options, function(err){
