a9f0b02c49507045efd362714ee46eafc4ec202e
Alberto Varela <alberto@berriart.com>
2016-03-18 01:12:45 +0100

    Adding Linux installer

--- samples/svc.sh
+++ samples/svc.sh
@@ -1,31 +1,27 @@
+#!/bin/bash
 
 export ACTION="$1"
 export OPTION="$2"
 
 function failed()
 {
-   local error=${1:-Undefined error}
-   echo "Failed: $error" >&2
-   exit 1
+  local error=${1:-Undefined error}
+  echo "Failed: $error" >&2
+  exit 1
 }
 
 function installsvc() {
-	export RUNAS=${USER}
+  export RUNAS=${USER}
 
-	sudo node install.js install ${RUNAS} ${OPTION}
+  sudo node install.js install ${RUNAS} ${OPTION}
 }
 
-function svcAction() { 
-	sudo node install.js $1 
+function svcAction() {
+  sudo node install.js $1
 }
 
 if [ "${ACTION}" == "install" ]; then
-	installsvc	 		
+  installsvc
 else
-	svcAction ${ACTION}
+  svcAction ${ACTION}
 fi
-
-
-
-
-
